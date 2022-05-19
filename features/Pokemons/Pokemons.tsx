d0fb9ef15419b000421cb391c9d25347f5c42254
Pavel7811 <62295683+Pavel7811@users.noreply.github.com>
2022-05-19 23:40:02 +0300

    залил на гит

---
+++ features/Pokemons/Pokemons.tsx
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from "react";
+import { FlatList, Text, View, StyleSheet } from "react-native";
+import { pokeAPI } from "../../services/pokeAPI";
+import { PokemonShortInfo } from "../../types/Pokemon";
+import { Pokemon } from "./components/Pokemon";
+
+export const Pokemons: React.FC = () => {
+  const [pokemons, setPokemons] = useState<PokemonShortInfo[]>([]);
+
+  const getPokemons = async () => {
+    try {
+      const pokemons = await pokeAPI.getPokemons();
+
+      setPokemons(pokemons);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    getPokemons();
+  }, []);
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Pokeapp</Text>
+      <FlatList
+        data={pokemons}
+        numColumns={2}
+        renderItem={({ item, index }) => (
+          <Pokemon index={index + 1} data={item} />
+        )}
+        style={styles.list}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 60,
+  },
+  title: {
+    textAlign: "center",
+    fontSize: 32,
+    fontWeight: "bold",
+  },
+  list: {
+    padding: 16,
+    marginTop: 16,
+  }
+});
---
+++ features/Pokemons/components/Pokemon/Pokemon.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import { StyleSheet, View, Text, Image } from "react-native";
+import { imageService } from "../../../../services/imageService";
+import { PokemonShortInfo } from "../../../../types/Pokemon";
+
+interface PokemonProps {
+  index: number;
+  data: PokemonShortInfo;
+}
+
+export const Pokemon: React.FC<PokemonProps> = ({ index, data }) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.name}>{data.name}</Text>
+      <Image
+        style={styles.image}
+        source={{ uri: imageService.getImageByIndex(index) }}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    width: "50%",
+    height: 400,
+    backgroundColor: "lightgrey",
+    marginBottom: 16,
+    borderRadius: 8,
+    padding: 16,
+    alignContent: 'center'
+    
+  },
+  name: {
+    textAlign: "center",
+    fontSize: 32,
+    textTransform: "capitalize",
+  },
+  image: {
+    width: "100%",
+    height: "50%",
+    alignContent: 'center'
+  },
+});
